280806f7de7a7096c4ae399c98078a7b1431005b
Polaaa5 <32522839+Polaaa5@users.noreply.github.com>
2018-02-18 00:29:53 +0500

    добавлен файл с работой

---
+++ файл.py
@@ -0,0 +1,16 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("<HTML>") 
+width1 = 700 
+height1 = 650
+border=7
+colorBG =("#808080") 
+print ("<title>")
+print ("Сборная России по хоккею")
+print ("</title>")
+print('<body style=background-color:', colorBG, ';>', sep='')
+print('<div align=center','><img src= "https://cdn1.img.rsport.ru/images/113276/80/1132768044.jpg" ', 'border=', border1, ' align=center',' width=50%',' height=50%', ' </div>', sep='') 
+print("Мужская сборная России по хоккею разгромила команду Словении в матче группового этапа на Олимпиаде в Южной Корее.") 
+print ("Сборная России набрала три очка в двух встречах. В заключительном матче группового этапа российские хоккеисты в субботу сыграют с командой США. Сборная Словения в этот же день встретится с командой Словакии.") 
+print('</body>') 
+print('</HTML>')
